17c82371a68317f21285366e409aa3c844eb409a
alexpikuta <olexandr.pikuta@gmail.com>
2018-11-18 14:48:07 +0200

    #9: Develop Home page

---
+++ src/pages/auth/Registration.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12>
+        <div>
+          <h3>Registration</h3>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ src/ui/logo/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1
+    class="logo"
+    :style="{'font-size': size + 'px'}"
+  >!D</h1>
+</template>
+ <script>
+   export default {
+     props: {
+       size: {
+         default: 99,
+         type: Number
+       }
+     }
+   }
+ </script>
+<style lang="sass" scoped>
+  .logo
+    display: inline-block
+    line-height: 1
+</style>
