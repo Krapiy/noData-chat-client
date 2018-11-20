794d2d5451122a26abd1398f370a09918d9ac65d
alexpikuta <olexandr.pikuta@gmail.com>
2018-11-20 12:55:52 +0200

    #6: Develop Login page

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -1,10 +1,41 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <div>
-          <h3>Login</h3>
-        </div>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="grey darken-2">
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid" validation>
+              <v-text-field
+                prepend-icon="person"
+                name="login"
+                label="Login"
+                type="text"
+                v-model.trim="login"
+                :rules="loginRules"
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                id="password"
+                type="password"
+                v-model="password"
+                :rules="pssswordRules"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              @click="onLogin"
+              :disabled="!valid"
+            >Login</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,5 +43,29 @@
 
 <script>
 export default {
+  data () {
+    return {
+      login: '',
+      password: '',
+      valid: false,
+      loginRules: [
+        v => !!v || 'Login is required'
+      ],
+      pssswordRules: [
+        v => !!v || 'Password is required'
+      ]
+    }
+  },
+  methods: {
+    onLogin () {
+      if (this.$refs.form.validate()) {
+        const user = {
+          login: this.login,
+          password: this.password
+        }
+        console.log(user)
+      }
+    }
+  }
 }
 </script>
